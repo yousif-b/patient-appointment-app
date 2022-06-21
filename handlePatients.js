c8a0218121222e5c10da572cffec74dfe9dbe51f
DeathByFork <yousifjabrallah@gmail.com>
2022-06-20 19:42:03 -0700

    started js

---
+++ handlePatients.js
@@ -0,0 +1,4 @@
+export default function handlePatients(patients){
+    renderPatients(patients);
+    renderAppointments(patients);
+}
--- script.js
+++ script.js
@@ -1,3 +1,35 @@
 import statsNavBtns from "./statsNavBtns.js";
+import handlePatients from "./handlePatients.js";
 
+let patients;
+
+if(!sessionStorage.getItem("patients")){
+    patients = [{
+        name: 'Rony Brawa',
+        age: 26,
+        dob: '05/23/1996',
+        height: 180,
+        weight: 87,
+        number: '32523157623',
+        medications: [],
+        appointments: [],
+    },
+    {
+        name: 'Rony Brawa',
+        age: 26,
+        dob: '05/23/1996',
+        height: 180,
+        weight: 87,
+        number: '32523157623',
+        medications: [],
+        appointments: [],
+    }]
+
+    sessionStorage.setItem("patients", JSON.stringify(patients));
+}
+else{
+    patients = JSON.parse(sessionStorage.getItem("patients"));
+}
+
+handlePatients(patients);
 statsNavBtns();
