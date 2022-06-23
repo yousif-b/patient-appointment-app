8970954e028dc0f32b59d92b2d19611ac85f6e42
DeathByFork <83885861+DeathByFork@users.noreply.github.com>
2022-06-22 19:06:48 -0700

    Medication and appointments js and css

--- patientPage.js
+++ patientPage.js
@@ -12,21 +12,39 @@ function getMedication(medication){
 }
 
 function getAppointments(appointments){
+    let appointmentsHTML = '';
+    appointments.forEach(a => {
+        appointmentsHTML += `
+        <tr>
+            <td> ${a.date} </td>
+            <td> ${a.time} </td>
+            <td> ${a.status} </td>
+        </tr>
+        `;
+    })
+    return appointmentsHTML;
 }
 
 patients.forEach(p =>{
     if(p.name == patientName){
         patientBox.innerHTML = `
             <h1> ${p.name} </h1>
-            <h2> Date of Birth: ${p.dob} </h2>
-            <h2> Height: ${p.height} </h2>
-            <h2> Weight: ${p.weight} </h2>
-            <h2> Sex: ${p.sex} </h2>
-            <h2> Number: ${p.number} </h2>
+            <div class = "patientInformation">
+                <h2> DOB: ${p.dob} </h2>
+                <h2> Height: ${p.height} </h2>
+                <h2> Weight: ${p.weight} </h2>
+                <h2> Sex: ${p.sex} </h2>
+                <h2> Number: ${p.number} </h2>
+            </div>
             <div class = 'medication'>
+            <h2>Medication: </h2>
             ${getMedication(p.medications)}
             </div>
             <div class = 'appointments'> 
+            <h2>Appointments: </h2>
+            <table>
+                ${getAppointments(p.appointments)}
+            </table>
             </div>
             <button> Edit </button>
             <button> Set Appointment </button>
