7121f4b26ca73eff13b8f90576beabf0595b51a1
yousif-b <yousifjabrallah@gmail.com>
2022-06-30 05:46:26 -0700

    changed patient cards ,patient page and added profile icon fetch
    overhauled the old table patient display and added a card list. also started using dicebear api to fetch profile icons

--- patientPage.js
+++ patientPage.js
@@ -28,31 +28,39 @@ function getAppointments(appointments){
 patients.forEach(p =>{
     if(p.name == patientName){
         patientBox.innerHTML = `
-            <h1> ${p.name} </h1>
-            <div class = "patientInformation">
-                <h2> DOB: ${p.dob} </h2>
-                <h2> Height: ${p.height} </h2>
-                <h2> Weight: ${p.weight} </h2>
-                <h2> Sex: ${p.sex} </h2>
-                <h2> Number: ${p.number} </h2>
-            </div>
-            <div id = "medicationAppointment"{
-                <div class = 'medication'>
-                    <h2>Medication: </h2>
-                    ${getMedication(p.medications)}
+            <div id = 'patientUpperSection'>
+                <div id = 'patientInformation'>
+                    <div id = 'patientName'>
+                        <div id = 'patientIcon'></div>
+                        <h1> ${p.name} </h1>
+                    </div>
+                    <div id = 'info'>
+                        <h2> DOB: ${p.dob} </h2>
+                        <h2> Height: ${p.height} </h2>
+                        <h2> Weight: ${p.weight} </h2>
+                        <h2> Sex: ${p.sex} </h2>
+                        <h2> Number: ${p.number} </h2>
+                        <div id = "patientButtons">
+                            <button id = "editPatientBtn"> Edit </button>
+                            <button id = "deletePatientBtn"> Delete Patient </button>
+                        </div>
+                    </div>
                 </div>
-                <div class = 'appointments'> 
-                    <h2>Appointments: </h2>
-                    <table>
-                        ${getAppointments(p.appointments)}
-                    </table>
-                </div>
+                <div id = 'appointments'> 
+                <h2>Appointments: </h2>
+                <table>
+                    ${getAppointments(p.appointments)}
+                </table>
             </div>
-            <div id = "patientButtons">
-                <button id = "editPatientBtn"> Edit </button>
-                <button id = "appointmentPatientBtn"> Set Appointment </button>
-                <button id = "deletePatientBtn"> Delete Patient </button>
             </div>
-        `
+            <div id = 'medication'>
+            <h2>Medication: </h2>
+            ${getMedication(p.medications)}
+            </div>
+        `;
     }
-})
+});
+
+fetch(`https://avatars.dicebear.com/api/male/${patientName}.svg`)
+.then(response => response.text())
+.then(svg => document.getElementById('patientIcon').insertAdjacentHTML("afterbegin", svg))
--- renderPatients.js
+++ renderPatients.js
@@ -1,15 +1,45 @@
 export default function renderPatients(patients){
     let patientList = document.getElementById('patientList');
-    patients.forEach(p => {
-        let tr = document.createElement('tr');
-        tr.className = 'patientRow';
-        tr.innerHTML = `<td>${p.name}</td>
-            <td>${p.dob}</td>
-            <td>${p.appointments[p.appointments.length-1].date}</td>
-            <td>${p.number}</td>`;
-        tr.addEventListener('click', () => {
+    patients.forEach((p) => {
+        let div = document.createElement('div');
+        div.className = 'patientCard';
+        div.innerHTML = `
+                <div class = "cardPatientIcon">
+                </div>
+                <div class = "cardInformationSection">
+                    <div class = "cardInformationLabels">
+                        <h3>Name</h3>
+                        <h3>DOB</h3>
+                        <h3>Phone</h3>
+                        <h3>Email</h3>
+                    </div>
+                    <div class = "cardInformationDetails">
+                        <h3>${p.name}</h3>
+                        <h3>${p.dob}</h3>
+                        <h3>${p.number}</h3>
+                        <h3>${p.address}</h3>
+                    </div>
+                </div>
+                <div class = "cardInformationSection">
+                    <div class = "cardInformationLabels">
+                        <h3>Latest Appt</h3>
+                        <h3>Height</h3>
+                        <h3>Weight</h3>
+                        <h3>Sex</h3>
+                    </div>
+                    <div class = "cardInformationDetails">
+                        <h3>${p.appointments[p.appointments.length-1].date}</h3>
+                        <h3>${p.height} cm </h3>
+                        <h3> ${p.weight} cm </h3>
+                        <h3>${p.sex}</h3>
+                    </div>
+                </div>
+            `;
+        
+        div.addEventListener('click', () => {
             window.location.href = `patient.html?p=${p.name}`;
         })
-        patientList.append(tr);
+        
+        patientList.append(div);
     })
 }
